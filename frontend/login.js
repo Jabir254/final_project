fb5d996eee69726bacfe5d9ebece88a31601dec2
jabir254 <jabirkullow@gmail.com>
2023-12-08 22:06:09 +0300

    updated all

--- frontend/login.js
+++ frontend/login.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  window.registerUser = async () => {
-    const username = document.getElementById("username").value;
+  window.loginUser = async () => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     try {
-      const response = await axios.post("/", {
-        // Assuming same-origin, use relative URLs
-        username,
+      const response = await axios.post("/api/users/login", {
         email,
         password,
       });
@@ -17,11 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
       // Store the token securely (e.g., in an HTTP-only cookie for added security)
       document.cookie = `token=${token}; secure; samesite=strict`;
 
-      console.log("User registered successfully. Token:", token);
+      console.log("User logged in successfully. Token:", token);
 
       // Redirect the user or perform other actions
     } catch (error) {
-      console.error("Error registering user:", error.response.data.msg);
+      console.error("Error logging in:", error.response.data.msg);
       // Handle error (e.g., display an error message to the user)
     }
   };
