248b668eec7157b52f1ee08343097c046b31b4a0
jabir254 <jabirkullow@gmail.com>
2023-12-12 19:54:11 +0300

    added get and get id routes

--- backend/routes/surveyRoutes.js
+++ backend/routes/surveyRoutes.js
@@ -6,7 +6,7 @@ const authentication = require("../components/auth/userAuthentication");
 //@route Post
 //create a new survey
 //@access Private(require authentication)
-router.post("/", async (req, res) => {
+router.post("/create", async (req, res) => {
   const { title, questions } = req.body;
 
   try {
@@ -25,5 +25,45 @@ router.post("/", async (req, res) => {
   }
 });
 
+// @route   GET /api/surveys
+// @desc    Get all surveys
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const surveys = await Survey.find().populate("creator", "username"); // Populate creator field with username
+    res.json(surveys);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET /api/surveys/:id
+// @desc    Get a specific survey by ID
+// @access  Public
+router.get("/:id", async (req, res) => {
+  const surveyId = req.params.id;
+
+  try {
+    const survey = await Survey.findById(surveyId).populate(
+      "creator",
+      "username"
+    ); // Populate creator field with username
+
+    if (!survey) {
+      return res.status(404).json({ msg: "Survey not found" });
+    }
+
+    res.json(survey);
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Survey not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
